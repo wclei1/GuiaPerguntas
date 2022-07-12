c0ac6b6bc04c6a74517a88aa846334586e82de32
Wellington Cleiton <wellingtonpinhofilho11@gmail.com>
2022-07-12 11:45:40 -0300

    Implementação do banco de dados MySQL

---
+++ database/Pergunta.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+//MODEL
+const Pergunta = connection.define('Perguntas',{
+    titulo:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descricao:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Pergunta.sync({force: false}).then(() => {
+    console.log('Tabela pergunta criada com sucesso!!')
+});
+
+module.exports = Pergunta;
--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser"); 
+const { urlencoded } = require("body-parser");
+const connection = require('./database/database');
+const Pergunta = require("./database/Pergunta")
 
-// Estou dizendo pro meu express usar o EJS como viw engine
+// DATABASE
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+
+
+// HABILITA USAR EJS NO EXPRESS COMO VIEW ENGINE
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+// BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+// ROTAS
 app.get("/", (req, res) => {
     res.render("index");
-    let titulo = document.getElementById('')
 });
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 });
+
+app.post("/salvarpergunta", (req, res)=>{
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
+});
+
 
 app.listen(8080, () => {console.log("App rodando!");});
